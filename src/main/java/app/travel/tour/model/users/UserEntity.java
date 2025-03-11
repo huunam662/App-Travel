4a20f03d260d02aeab5b816bf7c87788e78f5b1e
huunam662 <nguyen.hnam2024work@gmail.com>
2025-03-11 21:10:07 +0700

    cm new

--- src/main/java/app/travel/tour/model/users/UserEntity.java
+++ src/main/java/app/travel/tour/model/users/UserEntity.java
@@ -1,4 +1,15 @@
 package app.travel.tour.model.users;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
 }
